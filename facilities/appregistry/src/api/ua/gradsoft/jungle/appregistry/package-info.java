0115514e98d39a0cf9dfa11fc04579396d91dd49
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2009-02-18 08:21:56 +0000

    added publish.local task
    git-svn-id: http://datacenter.gradsoft.ua/repos/Jungle/jungle.public@14811 e6263924-d73c-0410-abf9-b8ad5fbdd136

---
+++ facilities/appregistry/src/api/ua/gradsoft/jungle/appregistry/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * ApplRegistry - simple registry of jungle applications in database.
+ *
+ *@see ua.gradsoft.jungle.appregistry.ApplicationRegistryFacade 
+ *@see ua.gradsoft.jungle.appregistry.AppRegistryFacadeBean
+ **/
+package ua.gradsoft.jungle.appregistry;
