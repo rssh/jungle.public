185bdcddeae0b0ac86082394d511e5cba67311a1
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2009-02-19 11:43:00 +0000

    snap
    git-svn-id: http://datacenter.gradsoft.ua/repos/Jungle/jungle.public@14823 e6263924-d73c-0410-abf9-b8ad5fbdd136

---
+++ platform/persistence/ejbqlao/src/main/java/ua/gradsoft/persistence/ejbqlao/util/NamedToPositionalParamsSqlTransformer.java
@@ -0,0 +1,78 @@
+package ua.gradsoft.persistence.ejbqlao.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import ua.gradsoft.persistence.ejbqlao.SqlPositionalQueryParams;
+
+
+public class NamedToPositionalParamsSqlTransformer
+{
+
+  /**
+   * translate ejb/sql 'native sql' query with named parameters to 'really native'
+   *  jdbc query with positional parameters.
+   * (note - this class does not touch options)
+   * @param nQuery - query with named parameters.
+   * @param namedParametets - parameters.
+   * @return sql query with positions parameters.
+   */
+  public static SqlPositionalQueryParams translate(String nQuery, Map<String,Object> namedParameters )
+  { 
+    StringBuilder sb = new StringBuilder();
+    List<Object> positionParameters = new ArrayList<Object>();
+    String[] ejbQueryParts = nQuery.split("'");
+    boolean inQuote=false;
+    for(int i=0; i<ejbQueryParts.length; ++i){
+        inQuote=((i%2)==1);
+        if (inQuote) {
+            sb.append("'");
+            sb.append(ejbQueryParts[i]);
+            sb.append("'");
+        }else{
+            substituteNamedParametersInPart(0,ejbQueryParts[i],sb,positionParameters,namedParameters);
+        }    
+    }
+    SqlPositionalQueryParams retval = new SqlPositionalQueryParams(sb.toString(),positionParameters);
+    return retval;
+  }
+  
+  /**
+   * substitue named parameters in string part, where we know that part does not contains string literals.
+   */
+  private static void substituteNamedParametersInPart(int index, String part, StringBuilder qb, List<Object> posParams, Map<String,Object> namedParams)
+  {
+    int sqlPositionParameterIndex=1;
+    int varIndex=index;
+    char[] arr = part.toCharArray();
+    while(varIndex < arr.length) {
+      while(arr[varIndex]!=':' && varIndex<arr.length) {
+          qb.append(arr[varIndex]);
+          ++varIndex;
+      }
+      if (varIndex!=arr.length) {
+          // we found begin of variable, lets search for end (first non-identifier)
+          boolean found=false;
+          int i=++varIndex;
+          for(; i<arr.length && !found; ++i) {
+              if (!Character.isLetterOrDigit(arr[i])) {
+                  found=true;
+              }
+          }
+          if (i==varIndex) {
+             throw new IllegalArgumentException("invalid sql query: ':' without name of variable");
+          }
+          String varName = part.substring(varIndex,i);
+          Object varValue = namedParams.get(varName);
+          if (varValue==null) {
+              throw new IllegalArgumentException("Sql valiable '"+varName+"' does not exists");
+          }
+          qb.append("?");
+          //qb.append(sqlPositionParameterIndex);
+          ++sqlPositionParameterIndex;
+          varIndex=i;
+      }
+    }
+  }
+
+}
