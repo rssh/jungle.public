f1f03ece3a77422a354b6f2d9429099b053b532e
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2009-02-27 17:50:43 +0000

    yet one hsqlk fix
    git-svn-id: http://datacenter.gradsoft.ua/repos/Jungle/jungle.public@14899 e6263924-d73c-0410-abf9-b8ad5fbdd136

--- platform/persistence/cluster_keys/sql/hsql/create_cluster_keys_hsql.sql
+++ platform/persistence/cluster_keys/sql/hsql/create_cluster_keys_hsql.sql
@@ -4,8 +4,8 @@ drop table my_cluster_node_info if exists;
 create table my_cluster_node_info
 (
  node_id      INTEGER  not null,
- org_id       INTEGER  not null
-  primary key(node_number, org_id)
+ org_id       INTEGER  not null,
+  primary key(node_id, org_id)
 );
 
 
