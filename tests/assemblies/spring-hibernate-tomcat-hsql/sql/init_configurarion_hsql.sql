890f5f9931110d293dbe0e9bae08ce4ba5d98248
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2009-06-04 10:03:54 +0000

    initial
    git-svn-id: http://datacenter.gradsoft.ua/repos/Jungle/jungle.public@15120 e6263924-d73c-0410-abf9-b8ad5fbdd136

---
+++ tests/assemblies/spring-hibernate-tomcat-hsql/sql/init_configurarion_hsql.sql
@@ -0,0 +1,6 @@
+
+insert into jungle_configitems(id,appname,name,descritpion,typecode,
+                               max_len, regexp, editable, value)
+ values(1,'app1','_1','first var',1,2,'',true,'04'); 
+
+
