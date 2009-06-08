b555c9b39434b8afcd396e991a9f557a35827cb7
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2009-06-08 20:15:26 +0000

    updated to use gxt-1.2.4
    git-svn-id: http://datacenter.gradsoft.ua/repos/Jungle/jungle.public@15153 e6263924-d73c-0410-abf9-b8ad5fbdd136

--- tests/assemblies/spring-hibernate-tomcat-hsql/gwt-ui/src/ua/gradsoft/jungle/testapp/client/GwtUI.java
+++ tests/assemblies/spring-hibernate-tomcat-hsql/gwt-ui/src/ua/gradsoft/jungle/testapp/client/GwtUI.java
@@ -4,91 +4,49 @@ import com.extjs.gxt.desktop.client.Desktop;
 import com.extjs.gxt.desktop.client.Shortcut;
 import com.extjs.gxt.desktop.client.StartMenu;
 import com.extjs.gxt.desktop.client.TaskBar;
-import com.extjs.gxt.ui.client.event.ComponentEvent;
-import com.extjs.gxt.ui.client.event.MenuEvent;
-import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.Window;
-import com.extjs.gxt.ui.client.widget.Window.CloseAction;
+import com.extjs.gxt.ui.client.widget.form.FormPanel;
 import com.extjs.gxt.ui.client.widget.menu.MenuItem;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.Widget;
 import ua.gradsoft.jungle.configuration.client.ConfigurationUI;
+import ua.gradsoft.jungle.gwt.util.client.GwtApplication;
 
 
 public class GwtUI implements EntryPoint
 {
 
+  private GwtApplication application;
   private Desktop desktop = new Desktop();
 
   public void onModuleLoad() {
 
-
-      SelectionListener<ComponentEvent> listener = new SelectionListener<ComponentEvent>()
-      {
-          @Override
-          public void componentSelected(ComponentEvent ce) {
-              Window w = null;
-              if (ce instanceof MenuEvent) {
-                  MenuEvent me = (MenuEvent)ce;
-                  w = me.item.getData("window");
-              } else {
-                  w = ce.component.getData("window");
-              }
-              if (!desktop.getWindows().contains(w)) {
-                  desktop.addWindow(w);
-              }
-              if (w!=null && !w.isVisible()) {
-                  w.show();
-              }else{
-                  w.toFront();
-              }
-          }
-      };
+       application = new GwtApplication();
+       application.setDesktop(desktop);
+       application.initAuth(GWT.getModuleBaseURL()+"/GWT-RPC/auth");
 
 
+       ConfigurationUI configurationUI = new ConfigurationUI(
+                      GWT.getModuleBaseURL()+"/GWT-RPC/ConfigurationFacade");
 
-       ConfigurationUI configurationUI = new ConfigurationUI();
-       configurationUI.init(GWT.getModuleBaseURL()+
-                              "/GWT-RPC/ConfigurationFacade");
 
-       Window configurationWindow = createConfigurationWindow(configurationUI);
-       //Widget w = configurationUI.getTableWidget("app2");
-     
-       Shortcut s1 = new Shortcut();
-       s1.setText("configuration");
-       s1.setId("configuration-win-shortcut");
-       s1.setData("window",configurationWindow);
-       s1.addSelectionListener(listener);
-       desktop.addShortcut(s1);
-       
+       LoginService loginService = new LoginService();
+          
        TaskBar taskbar = desktop.getTaskBar();
        
        StartMenu menu = taskbar.getStartMenu();
        menu.setHeading("test-ui");
        menu.setIconStyle("user");
-       
-       MenuItem menuItem = new MenuItem("Configuration");
-       menuItem.setData("window", configurationWindow);
-       menuItem.addSelectionListener(listener);
-       menu.add(menuItem);
+
+       application.addComponent(configurationUI);
+       application.addComponent(loginService);
+
        
        // does not touch RootPanel, desktop use div 'x-desktop'.
   }
 
 
-  private Window createConfigurationWindow(ConfigurationUI configurationUI)
-  {
-      Window w = new Window();
-      w.setCloseAction(CloseAction.CLOSE);
-      w.setMinimizable(true);
-      w.setMaximizable(true);
-      w.setHeading("Configuration for app2");
-      Widget tableWidget = configurationUI.getTableWidget("app2");
-      w.add(tableWidget);
-      return w;
-  }
+
 
 }
 
---
+++ tests/assemblies/spring-hibernate-tomcat-hsql/gwt-ui/src/ua/gradsoft/jungle/testapp/client/LoginService.java
@@ -0,0 +1,181 @@
+
+package ua.gradsoft.jungle.testapp.client;
+
+import com.extjs.gxt.desktop.client.Desktop;
+import com.extjs.gxt.desktop.client.StartMenu;
+import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
+import com.extjs.gxt.ui.client.event.ComponentEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.widget.MessageBox;
+import com.extjs.gxt.ui.client.widget.Window;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.form.FormPanel;
+import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.menu.MenuItem;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import java.util.HashMap;
+import java.util.Map;
+import ua.gradsoft.jungle.gwt.util.client.GwtApplication;
+import ua.gradsoft.jungle.gwt.util.client.GwtApplicationComponent;
+import ua.gradsoft.jungle.gwt.util.client.GwtUtils;
+
+/**
+ *Login form
+ * @author rssh
+ */
+public class LoginService extends GwtApplicationComponent
+{
+
+   public String getName()
+   { return "Login"; }
+
+
+   @Override
+   public void onRegistered(GwtApplication application)
+   {
+     application_=application;
+
+     Window loginWindow = createLoginWindow();
+     MenuItem menuItem = new MenuItem("Login");
+     menuItem.setData("window", loginWindow);
+     menuItem.addSelectionListener(application.getDesktopSelectionListener());
+     menuItem.setItemId("id-login");
+     application.getDesktop().getStartMenu().add(menuItem);    
+
+   }
+
+   void initPanel(FormPanel panel, Window topLoginFormWindow)
+   {
+      topLoginFormWindow_=topLoginFormWindow;   
+
+      panel.setHeading("Login");
+      panel.setFrame(true);
+      panel.setWidth(30*GwtUtils.getCurrentResolution().getXWidth());
+
+      final TextField<String> username = new TextField<String>();
+      username.setFieldLabel("Username");
+      username.setAllowBlank(false);
+      username.setMaxLength(14);
+      panel.add(username);
+
+      final TextField<String> password = new TextField<String>();
+      password.setFieldLabel("Password");
+      password.setAllowBlank(false);
+      password.setPassword(true);
+      password.setMaxLength(14);
+      panel.add(password);
+
+
+      panel.setButtonAlign(HorizontalAlignment.CENTER);
+
+      Button loginButton = new Button("Login");
+      loginButton.addSelectionListener(
+              new SelectionListener<ComponentEvent>() {
+               public void componentSelected(ComponentEvent e) {                   
+                   login_=username.getValue();
+                   password_=password.getValue();
+                   Map<String,String> loginParams = new HashMap<String,String>();
+                   loginParams.put("username", login_);
+                   loginParams.put("password", password_);
+                   application_.getAuthService().getSessionTicket("plain",
+                           loginParams, new AsyncCallback<String>(){
+                       public void onFailure(Throwable caught) {
+                           MessageBox.alert("Login failed.", caught.getMessage(), null);
+                       }
+
+                       public void onSuccess(String sessionTicket) {
+                           MessageBox.alert("successlogin", "success logim, sessionTicket="+sessionTicket, null);
+                           sessionTicket_=sessionTicket;
+                           topLoginFormWindow_.close();
+                           Desktop desktop = application_.getDesktop();
+                           if (desktop!=null) {
+                             loginToLogout(desktop.getStartMenu());
+                           }
+                           application_.markLogin(sessionTicket_);
+                       }
+
+                   }
+                   );                   
+               }
+      }
+              );
+
+      panel.addButton(loginButton);
+
+      Button cancelButton = new Button("Cancle");
+      cancelButton.addSelectionListener(
+              new SelectionListener<ComponentEvent>() {
+               public void componentSelected(ComponentEvent e) {                  
+                  topLoginFormWindow_.close();
+               }
+      }
+              );
+
+      panel.addButton(cancelButton);
+
+   }
+
+   public void   logout()
+   {
+     application_.getAuthService().logout(
+             new AsyncCallback<Void>(){
+
+            public void onFailure(Throwable caught) {
+                MessageBox.alert("", "error during logout:"+caught.getMessage(), null);
+            }
+
+            public void onSuccess(Void result) {
+                sessionTicket_=null;
+                application_.markLogout();
+            }
+
+     }
+             );
+   }
+
+   public String getSessionTicket()
+   { return sessionTicket_; }
+
+  private void loginToLogout(StartMenu startMenu)
+  {
+    MenuItem item = (MenuItem)startMenu.getItemByItemId("id-login");
+    item.setText("Logout");
+    item.setData("window", null);
+    item.addSelectionListener(
+            new SelectionListener<ComponentEvent>() {
+            @Override
+            public void componentSelected(ComponentEvent ce) {
+                Desktop desktop = application_.getDesktop();
+                logout();
+                if (desktop==null) {
+                    return;
+                }                          
+                MenuItem item = (MenuItem)desktop.getStartMenu().getItemByItemId("id-login");
+                item.setText("Login");
+                item.removeSelectionListener(this);
+                item.setData("window", topLoginFormWindow_);             
+            }
+    }
+    );
+  }
+
+  private Window createLoginWindow()
+  {
+      Window w = new Window();
+      w.setAutoWidth(true);
+      FormPanel loginPanel = new FormPanel();
+      initPanel(loginPanel, w);
+      w.add(loginPanel);
+      return w;
+  }
+
+
+
+   private String login_;
+   private String password_;
+   private String sessionTicket_;
+   private GwtApplication application_;
+   private Window  topLoginFormWindow_;
+  
+
+}
