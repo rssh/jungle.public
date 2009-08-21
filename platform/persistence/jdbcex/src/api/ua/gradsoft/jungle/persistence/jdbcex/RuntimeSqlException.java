7df6d600b1a2a621831a89c9e31ed2d860d7eb95
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2009-08-21 16:09:50 +0000

    1. added utility methods to RuntimeSqlException  2. added package-info
    git-svn-id: http://datacenter.gradsoft.ua/repos/Jungle/jungle.public@15468 e6263924-d73c-0410-abf9-b8ad5fbdd136

--- platform/persistence/jdbcex/src/api/ua/gradsoft/jungle/persistence/jdbcex/RuntimeSqlException.java
+++ platform/persistence/jdbcex/src/api/ua/gradsoft/jungle/persistence/jdbcex/RuntimeSqlException.java
@@ -9,9 +9,34 @@ import java.sql.SQLException;
 public class RuntimeSqlException extends RuntimeException
 {
 
+    /**
+     * create sql exception.
+     * @param ex
+     */
     public RuntimeSqlException(SQLException ex)
     {
       super(ex.getMessage(),ex);  
+    }
+
+    /**
+     * @return wrapped SQL Exception.
+     */
+    public SQLException getSQLException()
+    {
+      return (SQLException)getCause();
+    }
+
+    /**
+     * exception vendor-specific sql error code.
+     */
+    public int getVendorErrorCode()
+    {
+      return getSQLException().getErrorCode();
+    }
+
+    public String getSQLState()
+    {
+      return getSQLException().getSQLState();
     }
 
 }
