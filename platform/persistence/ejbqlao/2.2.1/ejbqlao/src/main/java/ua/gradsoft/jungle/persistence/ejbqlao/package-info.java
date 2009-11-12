aa9c315f750979a481cc45f427233a6fd675325a
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2009-11-12 00:41:48 +0000

    removed 2.2.1
    git-svn-id: http://datacenter.gradsoft.ua/repos/Jungle/jungle.public@15868 e6263924-d73c-0410-abf9-b8ad5fbdd136

--- platform/persistence/ejbqlao/2.2.1/ejbqlao/src/main/java/ua/gradsoft/jungle/persistence/ejbqlao/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 'ejbqlao' is a shortcut for EJB/QL Access Object.
- *
- * more or less universal access object, with remote api, 
- * which can be used as base for JPA DAO Objects.
- *@see CRUDFacade
- *@see EjbQlAccessObject
- **/
-package ua.gradsoft.jungle.persistence.ejbqlao;
