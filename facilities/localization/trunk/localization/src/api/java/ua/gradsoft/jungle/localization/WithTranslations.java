98b0ed5b89e3165e84cb5c61f4f74b3e6496e237
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2009-11-23 06:34:43 +0000

    addef rogotten
    git-svn-id: http://datacenter.gradsoft.ua/repos/Jungle/jungle.public@15963 e6263924-d73c-0410-abf9-b8ad5fbdd136

---
+++ facilities/localization/trunk/localization/src/api/java/ua/gradsoft/jungle/localization/WithTranslations.java
@@ -0,0 +1,16 @@
+
+package ua.gradsoft.jungle.localization;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ *
+ * @author rssh
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WithTranslations {
+
+    
+
+}
