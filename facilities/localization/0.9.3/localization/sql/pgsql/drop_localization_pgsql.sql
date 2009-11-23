c456bfa368b86f4482e08b2bdeec06d9844d0c58
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2009-11-23 06:30:11 +0000

    0.9.3 branched
    git-svn-id: http://datacenter.gradsoft.ua/repos/Jungle/jungle.public@15962 e6263924-d73c-0410-abf9-b8ad5fbdd136

---
+++ facilities/localization/0.9.3/localization/sql/pgsql/drop_localization_pgsql.sql
@@ -0,0 +1,20 @@
+
+drop table if exists localization_bundle_table_columns;
+
+drop table if exists localization_bundle_tables;
+
+drop table if exists localization_bundle_languages;
+
+drop table if exists localization_bundles;
+
+drop table if exists localization_type;
+
+drop table if exists languages_in_countries;
+
+drop table if exists countries_linfos;
+
+drop table if exists languages;
+
+
+
+  
---
+++ facilities/localization/0.9.3/localization/src/impl/java/ua/gradsoft/jungle/localization/PkTranslationTableColumn.java
@@ -0,0 +1,67 @@
+
+package ua.gradsoft.jungle.localization;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+
+/**
+ *Primary Key for associations between translation table and column
+ * @author rssh
+ */
+public class PkTranslationTableColumn implements Serializable
+{
+
+    @Column(name="table_name") // bug in hibernate
+    public String getTableName()
+    {
+      return tableName_;  
+    }
+
+    public void setTableName(String tableName)
+    {
+      tableName_=tableName;
+    }
+
+    @Column(name="column_prefix") // bug in hibernate
+    public String getColumnName()
+    {
+      return columnName_;
+    }
+
+    public void setColumnName(String columnName)
+    {
+      columnName_=columnName;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o instanceof PkTranslationTableColumn) {
+            PkTranslationTableColumn pko = (PkTranslationTableColumn)o;
+            if (getTableName()==null) {
+                if (pko.getTableName()==null) {
+                    return getColumnName().equals(pko.getColumnName());
+                }else{
+                    return false;
+                }
+            }else if (getTableName().equals(pko.getTableName())) {
+                return getColumnName().equals(pko.getColumnName());
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode()
+    {
+      return (((tableName_==null) ? 0 : tableName_.hashCode()) +
+              ((columnName_==null) ? 0 : columnName_.hashCode()));
+    }
+
+    private String tableName_;
+    private String columnName_;
+
+}
