07618347eff600ccc4a36f388c9fc972c75efc85
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2010-05-06 10:48:00 +0000

    fixed bug in checking for non-transient fields
    git-svn-id: http://datacenter.gradsoft.ua/repos/Jungle/jungle.public@16587 e6263924-d73c-0410-abf9-b8ad5fbdd136

--- platform/tools/phpjao/trunk/phpjao/testdata/t1/php/generated.php
+++ platform/tools/phpjao/trunk/phpjao/testdata/t1/php/generated.php
@@ -2,7 +2,11 @@
 ?>
 <?php class E1PHPJAOClassDescription extends PHPJAOClassDescription{
 public function __construct(){
+  parent::__construct();
+  
   $this->javaClass = 'ua.gradsoft.t1.E1';
+  
+  $this->phpClass = 'E1';
   
   $this->typesOfFields = array('name' => 'java.lang.String', 'value' => 'java.lang.String');
   
@@ -14,7 +18,7 @@ public function __construct(){
   }
  
  }
-class E1 extends PHPJAOBase{
+class E1 extends PHPJAOPOJOBase{
 static $phpjaoClassDescription;
  
  public function getPHPJAOClassDescription(){
