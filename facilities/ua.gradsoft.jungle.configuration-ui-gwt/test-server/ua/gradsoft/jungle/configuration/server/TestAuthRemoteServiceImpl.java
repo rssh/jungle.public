820ea7c7f33151664e2a21b397ca9745871f1ada
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2011-07-04 21:53:32 +0300

    new build scheme for configuration-ui--gwt

--- facilities/ua.gradsoft.jungle.configuration-ui-gwt/test-server/ua/gradsoft/jungle/configuration/server/TestAuthRemoteServiceImpl.java
+++ facilities/ua.gradsoft.jungle.configuration-ui-gwt/test-server/ua/gradsoft/jungle/configuration/server/TestAuthRemoteServiceImpl.java
@@ -3,6 +3,8 @@ package ua.gradsoft.jungle.configuration.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import java.util.Map;
+import java.util.List;
+import java.util.TreeMap;
 import ua.gradsoft.jungle.auth.client.AuthClientApiRemoteService;
 import ua.gradsoft.jungle.auth.client.AuthException;
 import ua.gradsoft.jungle.auth.client.ClientUserInfo;
@@ -23,6 +25,16 @@ public class TestAuthRemoteServiceImpl extends RemoteServiceServlet
     public boolean checkUserPermission(String sessionTicket, String permission, Map<String, String> params) throws InvalidSessionTicketException, AuthException {
         return true;
     }
+
+    public Map<String,Boolean> checkUserPermissions(String sessionTicker, List<String> permissions)
+    {
+      Map<String,Boolean> retval = new TreeMap<String,Boolean>();
+      for(String permission: permissions) retval.put(permission,true);
+      return retval;
+    }
+   
+
+
 
     public String getSessionTicket(String authType, Map<String, String> parameters) throws RedirectException, AuthException {
         return "xxx";
