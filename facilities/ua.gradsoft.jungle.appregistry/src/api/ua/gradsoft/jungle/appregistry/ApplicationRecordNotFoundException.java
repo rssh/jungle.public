69dbe544e6f3c57e9dbfe4c2453cb3699a2ee8d8
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2012-01-04 12:14:17 +0200

    removed unused

--- facilities/ua.gradsoft.jungle.appregistry/src/api/ua/gradsoft/jungle/appregistry/ApplicationRecordNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ua.gradsoft.jungle.appregistry;
-
-public class ApplicationRecordNotFoundException extends Exception
-{
- public ApplicationRecordNotFoundException(String name)
- {
-   super("application record not found for ("+name+")");
- }
-}
