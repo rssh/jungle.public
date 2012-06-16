a6013670d1a5d1c6acb771a7fbae7cb34f16edfa
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2012-06-16 12:53:59 +0300

    added scritp fopr finfing build.xml without target.javac set

---
+++ platform/tools/misc/find_untarget_javac.sh
@@ -0,0 +1,24 @@
+XALL=`find $1 -name 'build.xml'`
+echo "XALL=$XALL";
+if [ "$XALL" = "" ]
+then
+ echo "can't find build.xml files"
+ exit 1
+fi
+XJAVAC=`grep -l javac $XALL | sort -u | tee t.javac`
+echo "XJACAC=$XJAVAC";
+if [ "$XJAVAC" = "" ]
+then
+ echo "files with javac calls not found"
+ exit 1
+fi
+echo "XJAVAC=$XJAVAC";
+XJAVAC_TARGET=`grep -l javac.target $XJAVAC | sort -i | tee t.javac_target`
+if [ "$XJAVAC_TARGET" = "" ]
+then
+ echo "all files need to be updated"
+ echo $XJAVAC
+else
+ comm -2 -3 t.javac  t.javac_target 
+fi
+
